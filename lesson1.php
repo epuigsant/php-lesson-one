97434cdf49b03b721743455c2fdd41c46fe7ab01
Elsie Puig <epuigsant@gmail.com>
2016-11-19 10:44:53 -0800

    define variables

---
+++ lesson1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Lesson One</title>
+	</head>
+	<body>
+		
+		<?php 
+
+
+$friday = "Friday"; //assigned value of string Friday
+
+$todaysDate = date("Y.m.d"); //assigned value of a function
+
+$friday = "Monday" //same variable given different value
+	
+		
+		?>
+	
+	
+	</body>
+
+
+
+
+</html>
