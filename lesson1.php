f07660e3d636a3dec50e96bf6cf5a74c591f823a
Elsie Puig <epuigsant@gmail.com>
2016-11-19 10:51:47 -0800

    strings

--- lesson1.php
+++ lesson1.php
@@ -6,8 +6,9 @@
 	</head>
 	<body>
 		
+		
+		<h1>Variables</h1>
 		<?php 
-
 
 $friday = "Friday"; //assigned value of string Friday
 
@@ -16,6 +17,19 @@ $todaysDate = date("Y.m.d"); //assigned value of a function
 $friday = "Monday" //same variable given different value
 	
 		
+		?>
+		
+		<h1>Strings</h1>
+		
+		<?php 
+	
+$stringTest = "I am assigning this string to a variable names $stringTest";
+//This sentence is a string
+$stringTest2 = "string"; //a string can be a word, doesn't have to be a sentence
+$stringTest3 = "a 1 ewj...1/2 shsydynsknskans sjdbjdnjsdb"; //also a string
+
+echo "This is also a string - no variable needed";
+	
 		?>
 	
 	
